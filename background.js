db0106bbcc49f9bb9bccbc629d2af3d687932ba0
ZShab Niba <44338441+nibazshab@users.noreply.github.com>
2024-05-11 16:06:24 +0000

    update v1.1.0

--- background.js
+++ background.js
@@ -1,5 +1,16 @@
-chrome.downloads.onCreated.addListener(downloadItem => {
-    chrome.downloads.erase({ id: downloadItem.id });
+chrome.downloads.onDeterminingFilename.addListener((downloadItem) => {
+    chrome.downloads.cancel(downloadItem.id).then(() => {
+        if (chrome.runtime.lastError)
+            chrome.runtime.lastError = null;
+    });
+
+    if (downloadItem.finalUrl && downloadItem.finalUrl != "about:blank") {
+        downloadItem.url = downloadItem.finalUrl;
+    }
+
+    if (downloadItem.referrer == "about:blank") {
+        downloadItem.referrer = "";
+    }
 
     chrome.tabs.create({
         url: "data:text/plain," + encodeURIComponent(downloadItem.url)
